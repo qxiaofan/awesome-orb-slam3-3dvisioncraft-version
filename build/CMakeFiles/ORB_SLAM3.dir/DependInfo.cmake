5efcb78b38b4aff9bfbd7117c5e7e0f2a872b3da
qiyong <yong.qi@ecovacs.com>
2022-05-03 14:34:36 +0800

    去掉unneccary file

---
+++ build/CMakeFiles/ORB_SLAM3.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Atlas.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Atlas.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/CameraModels/KannalaBrandt8.cpp" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/CameraModels/KannalaBrandt8.cpp.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/CameraModels/Pinhole.cpp" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/CameraModels/Pinhole.cpp.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Converter.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Converter.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Frame.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Frame.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/FrameDrawer.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/FrameDrawer.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/G2oTypes.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/G2oTypes.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/ImuTypes.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/ImuTypes.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Initializer.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Initializer.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/KeyFrame.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/KeyFrame.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/KeyFrameDatabase.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/KeyFrameDatabase.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/LocalMapping.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/LocalMapping.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/LoopClosing.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/LoopClosing.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/MLPnPsolver.cpp" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/MLPnPsolver.cpp.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Map.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Map.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/MapDrawer.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/MapDrawer.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/MapPoint.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/MapPoint.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/ORBextractor.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/ORBextractor.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/ORBmatcher.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/ORBmatcher.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/OptimizableTypes.cpp" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/OptimizableTypes.cpp.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Optimizer.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Optimizer.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/PnPsolver.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/PnPsolver.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Sim3Solver.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Sim3Solver.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/System.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/System.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Tracking.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Tracking.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/TwoViewReconstruction.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/TwoViewReconstruction.cc.o"
+  "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/src/Viewer.cc" "/home/yong/workspace/02-github-projects/awesome-orb-slam3-3dvisioncraft-version/build/CMakeFiles/ORB_SLAM3.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "../include/CameraModels"
+  "/usr/local/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mono_inertial_euroc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mono_inertial_euroc.dir/Examples/Monocular-Inertial/mono_inertial_euroc.cc.o"
+  "../Examples/Monocular-Inertial/mono_inertial_euroc.pdb"
+  "../Examples/Monocular-Inertial/mono_inertial_euroc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mono_inertial_euroc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
